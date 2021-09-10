817600c46f55c8197ca10ca492619b83c5e40ad3
AntonMatyash <A.Matyash.FIT.121.20@knute.edu.ua>
2021-09-10 09:41:13 +0300

    second commit

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace LAB_WORK1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World!!!!!!!!!");
             Console.ReadKey();
         }
     }
